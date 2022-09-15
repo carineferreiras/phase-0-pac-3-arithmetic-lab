23a98926d28b7fc3ed28b121b38adb23c9008038
carine ferreira da silva <carine.silvaf1@gmail.com>
2022-09-15 15:11:21 -0300

    commit tree d0d4ad21a84c6f564c8f4c5e6fe6ea5aab200df9

---
+++ index.js
@@ -0,0 +1,58 @@
+function add(a,b) {
+    return "something";
+  }
+
+function subtract(a,b) {
+    return "something";
+  }
+
+function multiply(a,b) {
+    return "something";
+  }
+
+function divide(a,b) {
+    return "something";
+  }
+
+
+function add(a,b) {
+    return a+b;
+  }
+
+function subtract(a,b) {
+    return a-b;
+  }
+
+function multiply(a,b) {
+    return a*b;
+  }
+function divide(a,b) {
+    return a/b;
+  }
+
+function increment(n) {
+    n = n+1
+    return n 
+
+  }
+function decrement(n) {
+    n = n-1
+    return n 
+
+  }
+function makeInt(n){
+   const resumeOfBaseInt = parseInt(n,10);
+   console.log(resumeOfBaseInt);
+   return resumeOfBaseInt
+  }
+
+  makeInt(10)
+
+function preserveDecimal(n){
+    const resumeOfBaseInt = parseFloat(n);
+    console.log(resumeOfBaseInt);
+    return resumeOfBaseInt
+   }
+ 
+   makeInt(10)
+  
